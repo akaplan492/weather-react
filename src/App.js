dacf39f0471e478706e210f71f99311383111910
akaplan492 <68351916+akaplan492@users.noreply.github.com>
2020-08-21 16:01:51 -0400

    Updated App.css, index.css, and App.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello from React</h1>
-        <Weather city="New York" />
-      </header>
+      <Weather />
     </div>
   );
 }
