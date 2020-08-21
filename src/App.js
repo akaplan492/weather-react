2f04cc916cdca582d7daf7af491de3044ac68f99
akaplan492 <68351916+akaplan492@users.noreply.github.com>
2020-08-21 16:09:01 -0400

    Added axios call and alert to test axios

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Footer from "./Footer";
 function App() {
   return (
     <div className="App">
-      <Weather />
+      <Weather city="New York" />
       <Footer />
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,18 @@ import React from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The temperature in ${response.data.name} is ${response.data.main.temp}°F`
+    );
+  }
+
+  let apiKey = "44f5d434a5d71346c2c2e5e6ee86e65a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=imperial`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   let weatherData = {
     city: "New York",
     date: "Tuesday, July 21st 12:49PM",
