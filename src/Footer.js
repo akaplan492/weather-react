a1cc0c17f065f585435a9d32e22562b329876f74
akaplan492 <68351916+akaplan492@users.noreply.github.com>
2020-08-25 08:29:05 -0400

    Updated Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -2,11 +2,16 @@ import React from "react";
 import "./Footer.css";
 
 export default function Footer() {
-  let gitUrl = "https://github.com/akaplan492/Kaplan-SheCodesPlus-Project";
   return (
     <div className="Footer">
       <small>
-        <a href={gitUrl}>Open-source code</a> by Alexa Kaplan
+        <a
+          href="https://github.com/akaplan492/Kaplan-SheCodesPlus-Project"
+          target="_blank"
+        >
+          Open-source code
+        </a>{" "}
+        by Alexa Kaplan
       </small>
     </div>
   );
